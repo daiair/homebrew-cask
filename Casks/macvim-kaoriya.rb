cc68b3eaab2153b1ef8320f5e5539b943a4349bd
Miu <dai@Miu.local>
2014-04-23 21:50:50 +0900

    Add MacVim-KaoriYa

---
+++ Casks/macvim-kaoriya.rb
@@ -0,0 +1,8 @@
+class MacvimKaoriya < Cask
+  url 'https://github.com/splhack/macvim/releases/download/20140107/macvim-kaoriya-20140107.dmg'
+  homepage 'http://code.google.com/p/macvim-kaoriya/'
+  version '7.4.133'
+  sha256 '0a87ec0cb28e3b40eec6deb90e9e41ba44937f80de0c154cf80865414a6900b3'
+  link 'MacVim/MacVim.app'
+  binary 'MacVim/mvim'
+end
