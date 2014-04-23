fc0a2fe949a9a90e6fb17cf8cd3bfbecd8a1bd79
Miu <dai@Miu.local>
2014-04-23 19:15:48 +0900

    Add Google Chrome beta

---
+++ Casks/google-chrome-beta.rb
@@ -0,0 +1,7 @@
+class GoogleChrome < Cask
+  url 'https://dl.google.com/chrome/mac/beta/GoogleChrome.dmg'
+  homepage 'https://www.google.com/chrome/'
+  version 'latest'
+  no_checksum
+  link 'Google Chrome.app'
+end
