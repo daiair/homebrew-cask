a0bd884d52c99fdb0cbaf055dbbe585575e61149
Miu MATSUOKA <dai.air.ozora@gmail.com>
2014-10-14 09:51:34 +0900

    Rewrite the cask of Google Chrome beta.

--- Casks/google-chrome-beta.rb
+++ Casks/google-chrome-beta.rb
@@ -1,7 +1,26 @@
 class GoogleChromeBeta < Cask
+  version :latest
+  sha256 :no_check
+
   url 'https://dl.google.com/chrome/mac/beta/GoogleChrome.dmg'
   homepage 'https://www.google.com/chrome/'
-  version 'latest'
-  no_checksum
-  link 'Google Chrome.app'
+  license :unknown
+  tags :vendor => 'Google'
+
+  app 'Google Chrome.app'
+
+  zap :delete => [
+                  '~/Library/Application Support/Google/Chrome',
+                  '~/Library/Caches/Google/Chrome',
+                  '~/Library/Caches/com.google.Chrome',
+                  '~/Library/Caches/com.google.Chrome.helper.EH',
+                  '~/Library/Caches/com.google.Keystone.Agent',
+                  '~/Library/Caches/com.google.SoftwareUpdate',
+                  '~/Library/Google/GoogleSoftwareUpdate',
+                  '~/Library/Logs/GoogleSoftwareUpdateAgent.log',
+                 ],
+      :rmdir  => [
+                  '~/Library/Caches/Google',
+                  '~/Library/Google',
+                 ]
 end
--- Casks/macvim-kaoriya.rb
+++ Casks/macvim-kaoriya.rb
@@ -4,5 +4,4 @@ class MacvimKaoriya < Cask
   version '7.4.133'
   sha256 '0a87ec0cb28e3b40eec6deb90e9e41ba44937f80de0c154cf80865414a6900b3'
   link 'MacVim.app'
-  binary '/Applications/MacVim.app/Contents/MacOS/mvim'
 end
